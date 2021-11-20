781148a1a240e4e5aba8a0d85d045ae0a3737d3b
lhpchinaking <87065289+lhpchinaking@users.noreply.github.com>
2021-11-20 21:42:49 +0800

    Add files via upload

---
+++ Test1.java
@@ -0,0 +1,10 @@
+package com.haidi8.test1;
+
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("hellloword");
+        System.out.println("hellloword");
+        System.out.println("hellloword");
+        System.out.println("hellloword");
+    }
+}
---
+++ Test2.java
@@ -0,0 +1,7 @@
+package com.haidi8.test1;
+
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("xxxxxxx");
+    }
+}
